bbc7e524a72aa4fda31376dc60e5c10b9265dd90
Storn5 <stornstornstorn@gmail.com>
2020-04-10 17:05:07 +0300

    Added NavBar functionality

--- src/assets/js/navbar.js
+++
@@ -1,8 +0,0 @@
-function navBarFunc() {
-  var x = document.getElementById("meny");
-  if (x.className === "meny") {
-    x.className += "-responsive";
-  } else {
-    x.className = "meny";
-  }
-}
