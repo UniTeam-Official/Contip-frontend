6611b0e1ff6a1d5675f5ae77901092765168f023
Storn5 <stornstornstorn@gmail.com>
2020-04-10 15:11:02 +0300

    Fixed minor display issues with Login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import PasswordInputField from "./PasswordInputFields";
 import TextInputField from "./TextInputField";
+import SendButton from "./SendButton";
 
 class LoginForm extends Component {
   render() {
     return (
-	<div>
-	    <TextInputField />
-	    <PasswordInputField />
-	</div>
+      <div id="main">
+        <div class="inner">
+          <TextInputField />
+          <PasswordInputField />
+          <SendButton />
+        </div>
+      </div>
     );
   }
 }
