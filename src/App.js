8e64ad58519f9580eb93108410f4bf96fb41815d
Yyr3LL <tarasenko.yura.dr@gmail.com>
2020-04-13 21:24:39 +0300

    fixed more formatting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./components/Header";
 import MainPage from "./components/MainPage";
-import Footer from "./components/Footer";
 import LoginPage from "./components/LoginPage";
 import ProfilePage from "./components/ProfilePage";
 import WatchedMoviesPage from "./components/WatchedMoviesPage";
 import "./assets/css/main.css";
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Route path="/login/" component={LoginPage} />
-        <Route path="/profile/" component={ProfilePage} />
-        <Route exact path="/" component={MainPage} />
-        <Route path="/watched/" component={WatchedMoviesPage} />
-      </Router>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<Route path="/login/" component={LoginPage} />
+				<Route path="/profile/" component={ProfilePage} />
+				<Route exact path="/" component={MainPage} />
+				<Route path="/watched/" component={WatchedMoviesPage} />
+			</Router>
+		);
+	}
 }
 
 export default App;
