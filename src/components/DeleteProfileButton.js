fe85e0f9f9b130cfd59e6824688783c164abbe94
Storn5 <stornstornstorn@gmail.com>
2020-04-20 21:38:23 +0300

    Added input handlers and fixed components in ProfileForm

--- src/components/DeleteProfileButton.js
+++ src/components/DeleteProfileButton.js
@@ -5,8 +5,8 @@ class DeleteProfileButton extends Component {
 		return (
 			<div class="col-6 col-12-medium">
 				<ul class="actions stacked">
-					<li><a href="#" class="button small fit">Delete Account</a></li>
-					<li id="DeleteProfileButton"><a href="#" class="button small fit">Delete Account</a></li>
+					{/* <li><a href="#" class="button small fit">Delete Account</a></li> */}
+					<li id="DeleteProfileButton"><a href="#" class="button small fit" onClick={this.props.onClick} >Delete Account</a></li>
 				</ul>
 			</div>
 		);
