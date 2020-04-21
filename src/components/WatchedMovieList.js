869744334dc57fe2d7afc2cefe5c499929b54879
Storn5 <storn5@ukr.net>
2020-04-21 20:06:45 +0300

    Fixed WatchedMovieList movies & added routing for FilmPage

--- src/components/WatchedMovieList.js
+++ src/components/WatchedMovieList.js
@@ -2,24 +2,61 @@ import React, { Component } from "react";
 import Movie from "./Movie";
 
 class WatchedMovieList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [],
+			loaded: false,
+			placeholder: "Loading"
+		};
+	}
+
+	componentDidMount() {
+		const token = localStorage.getItem('jwt');
+		const options = {
+			method: "GET",
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+				'Authorization': `JWT ${token}`
+			}
+		}
+		fetch(`http://yyr3ll.pythonanywhere.com/api/v1/app/film/list`, options)
+			.then(response => {
+				console.log(response);
+				if (response.status > 400) {
+					return this.setState(() => {
+						return { placeholder: "Something went wrong!" };
+					});
+				}
+				return response.json();
+			})
+			.then(data => {
+				console.log(data);
+				this.setState(() => {
+					return {
+						data,
+						loaded: true
+					};
+				});
+			});
+	}
+
 	render() {
+		let films = <li></li>;
+		if (this.state && typeof(this.state.data.results) != 'undefined' && this.state.data.results.length > 0){
+			films = this.state.data.results.map(film => {
+				return (
+					<li><Movie link={`/film/${film.id}`} title={film.title} image="https://avatarfiles.alphacoders.com/139/139764.jpg" desc='GET DESCRIPTION FROM IMDB' genre={film.genre.map(genre => {return(genre.name + ' ')})} /></li>
+				);
+			})
+		}
 		return (
 			<div id="main">
 				<div className="inner">
 					<h1>Watched Movies</h1>
 					<ul>
-						<li>
-							<Movie link="/" title="First Movie" year="1991" desc="This man was a man but now hes a vovk. 3.14159265..." image="https://i.ytimg.com/vi/848q7WLmwbk/maxresdefault.jpg" />
-						</li>
-						<li>
-							<Movie link="/" title="Second Movie" year="2005" desc="Did you ever head the tragedy of Darth Plagueis the Wise? I thought not. It's not a story the Jedi would tell you. 3.14159265..." image="https://cdn.mos.cms.futurecdn.net/LSnvRUxYe64Lg7qrMzCGQd.jpg" />
-						</li>
-						<li>
-							<Movie link="/" title="Third Movie" year="1850" desc="This is the description of the third movie. It describes what the movie is about in a lot of unnecessary detail. 3.14159265..." image="https://tul.imgix.net/content/article/april-movies.jpg?auto=format,compress&w=1200&h=630&fit=crop" />
-						</li>
-						<li>
-							<Movie link="/" title="Fourth Movie" year="2020" desc="This man was a man but now hes a vovk. 3.14159265..." image="https://regalcdn.azureedge.net/BadBoysforLife/HO00009996/TV_SmallPosterImage/20191125-090126975.jpg" />
-						</li>
+						{films}
 					</ul>
 				</div>
 			</div>
