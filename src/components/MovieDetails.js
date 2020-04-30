aaf68b9713808449ce15878a0342335620c56c96
Storn5 <storn5@ukr.net>
2020-04-30 17:40:33 +0300

    Fixed WatchedFilmsPage

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -4,8 +4,8 @@ class MovieDetails extends Component {
 	render() {
 		return (
 			<div className="content">
-				<h3> Description: </h3>
-				<p>{this.props.desc}</p>
+				{/*<h3> Description: </h3>
+				<p>{this.props.desc}</p>*/}
 				<h3> Genres: </h3>
 				<p>{this.props.genre}</p>
 			</div>
